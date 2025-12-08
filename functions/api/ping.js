d1a3b5c2dd9c4f58cbf6a9ba52eba77cc56e19ff
AurraLynn <141542221+AurraLynn@users.noreply.github.com>
2025-12-09 01:28:49 +0800

    Update ping.js

--- functions/api/ping.js
+++ functions/api/ping.js
@@ -1,10 +1,1 @@
-export default {
-  async fetch() {
-    return new Response("API_PING_OK_V1", {
-      headers: {
-        "content-type": "text/plain; charset=utf-8",
-        "cache-control": "no-store",
-      },
-    });
-  },
-};
+文字
